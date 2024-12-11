d6dcd10107fdf682b4bd56ae77340445ac77d53e
Raflot <swann.piau@ensea.fr>
2024-12-11 16:57:53 +0100

    3

--- enseash.c
+++ enseash.c
@@ -1,6 +1,43 @@
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
-int main(int argc, char *argv[]){
-    printf("Hello world\n");
+int main(int argc, char *argv[])
+{
+    // write the message with write
+    const char *message = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+    size_t lenmsg = strlen(message);
+    write(1, message, lenmsg);
+    // Define the prompt
+    char *prompt = "ensea %";
+
+    // show the prompt
+    size_t lenprompt = strlen(prompt);
+    write(1, prompt, lenprompt);
+
+    // read the imput
+    char input[256];
+    fgets(input, 256, stdin);
+
+    while (1) {
+        // Show the prompt
+        size_t lenprompt = strlen(prompt);
+        write(1, prompt, lenprompt);
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break; // exit if the end is reached to avoid repetition
+        }
+
+        // Remove the newline character from the input
+        size_t lenimp = strlen(input);
+        if (input[lenimp - 1] == '\n') {
+            input[lenimp - 1] = '\0';
+        }
+        //exit command
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+    }
+
 }
