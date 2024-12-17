55a7bc107fcf101abb87234377113e8c9bf2bdcf
Raflot <swann.piau@ensea.fr>
2024-12-17 16:14:55 +0100

    TP1_partie2_q5

--- enseash.c
+++ enseash.c
@@ -4,13 +4,14 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
     char input[128];
     pid_t pid;
     int status;
     char prompt[64] = "enseash % ";
-
+    struct timespec start_time, end_time;
     write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\nTapez 'exit' pour quitter.\n", 59);
 
     while (1) {
@@ -35,7 +36,7 @@ int main(int argc, char *argv[]) {
             write(STDOUT_FILENO, fortune_msg, strlen(fortune_msg));
             continue; //return to prompt
         }
-
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
         pid = fork();
         if (pid == 0) {
             //child, execute the command
@@ -48,12 +49,13 @@ int main(int argc, char *argv[]) {
         } else if (pid > 0) {
             //parent wait
             waitpid(pid, &status, 0);
-
+            clock_gettime(CLOCK_MONOTONIC, &end_time);
+            long time = (end_time.tv_sec - start_time.tv_sec) * 1000 + (end_time.tv_nsec - start_time.tv_nsec) / 1000000;
             //update prompt
             if (WIFEXITED(status)) {
-                snprintf(prompt, sizeof(prompt), "enseash [exit:%d] %% ", WEXITSTATUS(status));
+                snprintf(prompt, sizeof(prompt), "enseash [exit:%d|%ldms] %% ", WEXITSTATUS(status), time);
             } else if (WIFSIGNALED(status)) {
-                snprintf(prompt, sizeof(prompt), "enseash [sign:%d] %% ", WTERMSIG(status));
+                snprintf(prompt, sizeof(prompt), "enseash [sign:%d|%ldms] %% ", WTERMSIG(status), time);
             }
         } else {
             //error
