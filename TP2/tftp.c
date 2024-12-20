35532336b620f74ee5ddb1d9013516c8774ad8bc
Raflot <swann.piau@ensea.fr>
2024-12-20 10:33:17 +0100

    q2-3

--- TP2/tftp.c
+++ TP2/tftp.c
@@ -1,25 +1,54 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define TFTP_PORT 69
+#define TFTP_PORT "69"
 #define BUFFER_SIZE 512
 
-int main(int argc, char *argv[])
-{
-
-    const char *server_ip = "10.10.32.11";
-    const char *filename = "haa.txt";
-    const char *mode = "Octet";
-
-    // UDP socket
-    int soc = socket(AF_INET, SOCK_DGRAM, 0);
-    if (soc = -1)
-    {
-        perror("error");
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("error");
+        return EXIT_FAILURE;
     }
 
+    char *host = argv[1];
+    char *filename = argv[2];
+    
+    struct addrinfo hints, *res;
+    int soc, info;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;       // IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // UDP
+
+    //addrinfo
+    info = getaddrinfo(host, TFTP_PORT, &hints, &res);
+    if (info != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(info));
+        return EXIT_FAILURE;
+    }
+
+    //UDP socket
+    soc = socket(AF_INET, SOCK_DGRAM, 0);
+    if (soc == -1) {
+        perror("error");
+        return EXIT_FAILURE;
+    }
+
+    printf("socket created successfully. file send to: %s\n", host);
+
     close(soc);
+    freeaddrinfo(res);
+
+    return EXIT_SUCCESS;
 }
